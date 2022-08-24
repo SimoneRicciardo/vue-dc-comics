55945fdd88a394b99d184212fb670565c261d36d
simone ricciardo <you@example.com>
2022-08-24 17:07:02 +0200

    scheletro da finire footer e scss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <MyHeader/>
+      <MyMain/>
+      <FooterTop/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue';
+import MyMain from './components/MyMain.vue';
+import FooterTop from './components/FooterTop.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MyHeader,
+    MyMain,
+    FooterTop
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/FooterTop.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(link, index) in LinkFooter" :key="index" >
+                <img :src="link.img" :alt="link.text">
+                <p>{{link.text}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterTop',
+    data() {
+        return{
+            LinkFooter: [
+                {
+                    text: 'DIGITAL COMICS',
+                    img: '../assets/img/buy-comics-digital-comics.png',
+                },
+                {
+                    text: 'DC MERCHANDISE',
+                    img: '../assets/img/buy-comics-merchandise.png',
+                },
+                {
+                    text: 'SUBSCRIPTION',
+                    img: '../assets/img/buy-comics-subscriptions.png',
+                },
+                {
+                    text: 'COMIC SHOP LOCATOR',
+                    img: '../assets/img/buy-comics-shop-locator.png',
+                },
+                {
+                    text: 'DC POWER VISA',
+                    img: '../assets/img/buy-dc-power-visa.svg',
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
